1f276b0ce5883ed7e1871b3999a1c8243fda56ee
Elia Vandini <vandini.elia@gmail.com>
2026-04-13 13:37:56 +0200

    reformatted algorythm

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,10 +1,14 @@
-import pyscreenshot
-from PIL import ImageGrab
+import random
+import time
+
+from tqdm import tqdm
+
 
 class sudokuGrid:
     raw = []
+    rating: str
 
-    def __init__(self, rawInput: str):
+    def __init__(self, rawInput: str, rating: str = "0.0"):
         """
         main class for sudoku numbermanaging
         :param rawInput: generates a sudoku grid based
@@ -12,12 +16,12 @@ class sudokuGrid:
         are the numbers from left to right of the rows
         of a normal sudokugrid, starting from the top
         """
-        output = ""
-        for i in rawInput:
-            output = f"{output}{i},"
-
-        output = output[:-1]
-        self.raw = list(map(int, output.split(',')))
+        # output = ""
+        # for i in rawInput:
+        #     output = f"{output}{i},"
+        # output = output[:-1]
+        self.raw = [int(i) for i in rawInput]
+        self.rating = rating
 
     def setLine(self, Input, line):
         """
@@ -25,14 +29,14 @@ class sudokuGrid:
         :param line: represents the line to be modified
         :param Input: represents the Input List witch contains 9 caracters
         """
-        self.raw[line * 9:((line * 9) + 9)] = Input
+        self.raw[line * 9 : ((line * 9) + 9)] = Input
 
     def getLine(self, line):
         """
         returns a list containing the values of a given line
         :param line: indicates the line to return
         """
-        return self.raw[line * 9:((line * 9) + 9)]
+        return self.raw[line * 9 : ((line * 9) + 9)]
 
     def setColumn(self, Input, column):
         """
@@ -50,160 +54,286 @@ class sudokuGrid:
         returns a list containing the values of a given column
         :param column: indicates the column to return
         """
-        return self.raw[column: 81: 9]
+        return self.raw[column:81:9]
 
-    def setCube(self, Input, cube):
+    def setSquare(self, Input, square):
         """
         set a colums to a given list
-        :param cube: represents the cube to be modified
+        :param square: represents the square to be modified
         :param Input: represents the Input List witch contains 9 caracters
         """
-        f = ((cube // 3) * 27) + ((cube % 3) * 3)
-        self.raw[f:f + 3] = Input[0:3]
-        self.raw[f + 9:f + 12] = Input[3:6]
-        self.raw[f + 18:f + 21] = Input[6:9]
+        f = ((square // 3) * 27) + ((square % 3) * 3)
+        self.raw[f : f + 3] = Input[0:3]
+        self.raw[f + 9 : f + 12] = Input[3:6]
+        self.raw[f + 18 : f + 21] = Input[6:9]
 
-    def getCube(self, cube):
+    def getSquare(self, square):
         """
-        returns a list containing the values of a given Cube
-        :param Cube: indicates the column to return
+        returns a list containing the values of a given Square
+        :param Square: indicates the column to return
         """
-        f = ((cube // 3) * 27) + ((cube % 3) * 3)
-        return self.raw[f:f + 3] + self.raw[f + 9:f + 12] + self.raw[f + 18:f + 21]
+        f = ((square // 3) * 27) + ((square % 3) * 3)
+        return (
+            self.raw[f : f + 3] + self.raw[f + 9 : f + 12] + self.raw[f + 18 : f + 21]
+        )
 
-    def printCube(self):
-        print(' ')
+    # def setCell(self, x: int, y: int, val: int):
+
+    def completeLine(self, index):
+        missing_index = -1
+        missing = -1
+        for i, t in enumerate(self.getLine(index)):
+            if t != 0:
+                if missing_index != -1:
+                    return
+                missing_index = i
+                missing = t
+
+        if missing_index != -1:
+            return
+        self.raw[getRawPos(index, missing_index, "l")] = missing
+
+    def completeColumn(self, index):
+        missing_index = -1
+        missing = -1
+        for i, t in enumerate(self.getLine(index)):
+            if t != 0:
+                if missing_index != -1:
+                    return
+                missing_index = i
+                missing = t
+
+        if missing_index != -1:
+            return
+        self.raw[getRawPos(index, missing_index, "c")] = missing
+
+    def completeSquare(self, index):
+        missing_index = -1
+        missing = -1
+        for i, t in enumerate(self.getLine(index)):
+            if t != 0:
+                if missing_index != -1:
+                    return
+                missing_index = i
+                missing = t
+
+        if missing_index != -1:
+            return
+        self.raw[getRawPos(index, missing_index, "s")] = missing
+
+    def printGrid(self):
+        print(" ")
         for i in range(9):
             print(self.getLine(i))
-        print(' ')
+        print(" ")
 
-class util:
+    def solved(self):
+        if self.raw.count(0) > 0:
+            return False
+        for i in range(0, 9):
+            if (
+                not isStraight(self.getColumn(i))
+                or not isStraight(self.getSquare(i))
+                or not isStraight(self.getLine(i))
+            ):
+                return False
+        return True
 
-    @staticmethod
-    def getMissing(Input):
-        """
-        returns a list containing the numbers from 1 to 9 missing in Input
-        :param Input: a 9 number long list of numbers to be searched
-        """
-        param = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        out = []
-        for t in param:
-            if not Input.count(t) > 0:
-                out.append(t)
-        return out
+    def solve(self):
 
-    @staticmethod
-    def getMissingPos(Input):
-        """
-        :param Input: a 9 number long list of numbers to be searched
-        :return returns a list containing all positions of 0
-        """
-        indices = []
-        for e in range(len(Input)):
-            if Input[e] == 0:
-                indices.append(e)
-        return indices
+        ok0 = True
+        while ok0:
+            sudoku_hash = self.__hash__()
 
-    @staticmethod
-    def getRawPos(subIndex, Index, mode="r"):
-        """
-        :param subIndex: Index of the subclass
-        :param Index: Index of the searched number inside teh subclass
-        :param mode: type of subclass to search ["r" = raw, "l" = Line, "c" = Column, "C" = Cube]
-        :return: returns the position of Index inside sudokuSolver.raw
-        """
-        if mode == "r":
-            return Index
-        elif mode == "l":
-            return (subIndex * 9) + Index
-        elif mode == "c":
-            return (Index * 9) + subIndex
-        elif mode == "C":
-            f = ((subIndex // 3) * 27) + ((subIndex % 3) * 3)
-            if Index < 3:
-                return f + (Index % 3)
-            elif Index < 6:
-                return f + (Index % 3) + 9
-            else:
-                return f + (Index % 3) + 18
+            for i in range(0, 9):
+                self.completeLine(i)
+                self.completeColumn(i)
+                self.completeSquare(i)
 
-    @staticmethod
-    def getLine(RawIndex):
-        """
-        :param RawIndex: Index to be searched
-        :return: The Line the RawIndex touches
-        """
-        return RawIndex // 9
+            for i in range(0, 9):
+                square = self.getSquare(i)
+                for missing in getMissing(square):
+                    available = -1
+                    for target_index, target_cell in enumerate(square):
+                        if target_cell != 0:
+                            continue
+                        line = self.getLine((i - (i % 3)) + (target_index // 3))
+                        column = self.getColumn(((i * 3) % 9) + (target_index % 3))
+                        if line.count(missing) <= 0 and column.count(missing) <= 0:
+                            if available != -1:
+                                available = -1
+                                break
+                            available = target_index
+                    if available != -1:
+                        # print(self.raw[getRawPos(i, available, "C")])
+                        self.raw[getRawPos(i, available, "s")] = missing
 
-    @staticmethod
-    def getColumn(RawIndex):
-        """
-        :param RawIndex: Index to be searched
-        :return: The Column the RawIndex touches
-        """
-        return RawIndex % 9
+            # self.printGrid()
+            if sudoku_hash == self.__hash__():
+                break
 
-    @staticmethod
-    def getCube(RawIndex):
-        """
-        :param RawIndex: Index to be searched
-        :return: The Cube the RawIndex touches
-        """
-        return ((RawIndex // 9) // 3) * 3 + (RawIndex % 9) // 3
-
-    @staticmethod
-    def listOverride(InputList, Input, Index):
-        """
-        :param InputList: Original List
-        :param Input: List to be inserted
-        :param Index: Index from where to start to overridden
-        :return: returns a List of the sae lenght of Inputlist but
-        the Items contained in it are overwritten by the Items of
-        the second list, starting by a given Index
-        """
-        return InputList[:Index] + Input[Index - len(InputList) - 1:len(InputList) - Index] + InputList[Index + len(Input):]
-
-    @staticmethod
-    def fillMissing(Input):
-        """
-        Automaticly fills in the last missing gap. won't work
-        if there is more than one ore none
-        :param Input: Input list of 9 numbers
-        :return: A full list from the Input
-        """
-        if len(util.getMissing(Input)) == 1:
-            return util.listOverride(Input, util.getMissing(Input), util.getMissingPos(Input)[0])
-        return Input
+    def __hash__(self) -> int:
+        return hash("".join(str(i) for i in self.raw))
 
 
+def isStraight(Input: list[int]):
+    for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        if Input.count(i) != 1:
+            return False
+    return True
 
 
-screen = read(ImageGrab.grab(317, 243, 906, 832))
+def getMissing(Input):
+    """
+    returns a list containing the numbers from 1 to 9 missing in Input
+    :param Input: a 9 number long list of numbers to be searched
+    """
+    param = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    out = []
+    for t in param:
+        if not Input.count(t) > 0:
+            out.append(t)
+    return out
 
 
-sudoku = sudokuGrid('400962000760005020008070001050006800004003106300000005002000984800000200509027000')
-sudoku.printCube()
-ok0 = True
-while ok0:
-    sudoku1 = sudoku
+def getMissingPos(Input):
+    """
+    :param Input: a 9 number long list of numbers to be searched
+    :return returns a list containing all positions of 0
+    """
+    indices = []
+    for e in range(len(Input)):
+        if Input[e] == 0:
+            indices.append(e)
+    return indices
 
-    for i in range(0, 9):
-        sudoku1.setLine(util.fillMissing(sudoku1.getLine(i)), i)
-        sudoku1.setCube(util.fillMissing(sudoku1.getCube(i)), i)
-        sudoku1.setColumn(util.fillMissing(sudoku1.getColumn(i)), i)
 
-    for a in range(0, 9):
-        for b in util.getMissing(sudoku1.getLine(a)):
-            temp = []
-            for c in util.getMissingPos(sudoku1.getLine(a)):
-                rawPos = util.getRawPos(a, c, "l")
-                if sudoku1.getCube(util.getCube(rawPos)).count(b) == 0 and sudoku1.getColumn(util.getColumn(rawPos)).count(b) == 0:
-                    temp.append(c)
-            if len(temp) == 1:
-                sudoku1.setLine(util.listOverride(sudoku1.getLine(a), [b], temp[0]), a)
+def getRawPos(subIndex: int, index: int, mode="r") -> int:
+    """
+    :param subIndex: Index of the subclass
+    :param Index: Index of the searched number inside teh subclass
+    :param mode: type of subclass to search ["r" = raw, "l" = Line, "c" = Column, "C" = Square]
+    :return: returns the position of Index inside sudokuSolver.raw
+    """
+    if mode == "r":
+        return index
+    elif mode == "l":
+        return (subIndex * 9) + index
+    elif mode == "c":
+        return (index * 9) + subIndex
+    elif mode == "s":
+        f = ((subIndex // 3) * 27) + ((subIndex % 3) * 3)
+        if index < 3:
+            return f + (index % 3)
+        elif index < 6:
+            return f + (index % 3) + 9
+        else:
+            return f + (index % 3) + 18
+    return index
 
-    if sudoku1.raw == sudoku.raw:
-        ok0 = False
-    sudoku = sudoku1
 
-sudoku.printCube()
+def getLine(RawIndex):
+    """
+    :param RawIndex: Index to be searched
+    :return: The Line the RawIndex touches
+    """
+    return RawIndex // 9
+
+
+def getColumn(RawIndex):
+    """
+    :param RawIndex: Index to be searched
+    :return: The Column the RawIndex touches
+    """
+    return RawIndex % 9
+
+
+def getSquare(RawIndex):
+    """
+    :param RawIndex: Index to be searched
+    :return: The Square the RawIndex touches
+    """
+    return ((RawIndex // 9) // 3) * 3 + (RawIndex % 9) // 3
+
+
+def listOverride(InputList, Input, Index):
+    """
+    :param InputList: Original List
+    :param Input: List to be inserted
+    :param Index: Index from where to start to overridden
+    :return: returns a List of the sae lenght of Inputlist but
+    the Items contained in it are overwritten by the Items of
+    the second list, starting by a given Index
+    """
+    return (
+        InputList[:Index]
+        + Input[Index - len(InputList) - 1 : len(InputList) - Index]
+        + InputList[Index + len(Input) :]
+    )
+
+
+def fillMissing(Input):
+    """
+    Automaticly fills in the last missing gap. won't work
+    if there is more than one ore none
+    :param Input: Input list of 9 numbers
+    :return: A full list from the Input
+    """
+    if len(getMissing(Input)) == 1:
+        return listOverride(Input, getMissing(Input), getMissingPos(Input)[0])
+    return Input
+
+
+def solve_db():
+    modes = ["easy", "medium", "hard", "diabolical"]
+    # modes = ["easy"]
+    sudokus: list[sudokuGrid] = []
+    for sudoku_mode in modes:
+        with open(f"sudoku-exchange-puzzle-bank/{sudoku_mode}.txt") as databse_file:
+            for num, line in enumerate(databse_file):
+                if random.randint(0, 50) > 1:
+                    continue
+                line = line.strip().split(" ")
+                sudokus.append(sudokuGrid(line[1], line[3]))
+
+    out = []
+    start_time = time.time()
+    prog_bar = tqdm(
+        total=len(sudokus),
+    )
+    for sudoku in sudokus:
+        sudoku.solve()
+        out.append((sudoku.rating, sudoku.solved()))
+        prog_bar.update()
+
+    end_time = time.time()
+    ratings: dict = {}
+    total_sum = 0
+    for res in out:
+        rate = ratings.get(res[0], (0, 0))[0]
+        count = ratings.get(res[0], (0, 0))[1]
+        new_rate = (rate * count + int(res[1])) / (count + 1)
+        ratings[res[0]] = (new_rate, count + 1)
+        if res[1]:
+            total_sum += 1
+
+    print("Performance per rating: ")
+    for k, v in sorted(ratings.items(), key=lambda x: float(x[0])):
+        print(f"{k}: {round(v[0] * 100, 2)}% ({v[1]} total)")
+    print(
+        f"overall: {round((total_sum / len(out)) * 100, 2)}% ({len(out)} total) in {end_time - start_time} seconds ({(round(end_time - start_time, 2)) / len(out)} per sudoku)"
+    )
+
+
+def solve_simple():
+    sudoku = sudokuGrid(
+        "050703060007000800000816000000030000005000100730040086906000204840572093000409000"
+    )
+    sudoku.printGrid()
+    sudoku.solve()
+    sudoku.printGrid()
+    print(f"solved: {sudoku.solved()}")
+
+
+# solve_simple()
+solve_db()
